dd314f8b92c7ebc49ac58481372761ffc4f9cd53
FinlayS <fsgm01@gmail.com>
2018-10-31 10:12:49 +0000

    S7 L67 adding delete function

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -1,10 +1,6 @@
 //const MongoClient = require('mongodb').MongoClient;
 const {MongoClient, ObjectID} = require('mongodb');
 
-var obj = new ObjectID();
-console.log(obj)
-
-
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     if (err) {
         return console.log('Unable to connect to MongoDB server');
@@ -34,7 +30,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     db.collection('Users').find({name: 'Sheenar'}).toArray().then((docs) => {
         console.log('Users');
         console.log(JSON.stringify(docs, undefined, 2))
-        
+
     }, (err) => {
         console.log('Cant get doc', err);
     })
