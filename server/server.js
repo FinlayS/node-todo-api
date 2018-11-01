8cfe4db60d3fff8f6b5a7471dc67ae0191b2a79d
FinlayS <fsgm01@gmail.com>
2018-11-01 09:20:47 +0000

    S7 L71 validators types and defaults

--- server/server.js
+++ server/server.js
@@ -5,30 +5,50 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
+
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
-var newTodo = new Todo({
-    text : 'Cook dinner'
-});
-
-newTodo.save().then(() => {
-    console.log('There saved')
-}, (e) => {
-    console.log('There was an error')
+var User =mongoose.model('User', {
+    email: {
+        type: String,
+        minlength: 1,
+        required: true,
+        trim: true
+    }
 })
 
-var anotherNewTodo = new Todo({
-    text: 'Complete challange',
-    completed: true,
-    completedAt: 850
+// var anotherNewTodo = new Todo({
+//     text: '    returns      '
+// });
+//
+// anotherNewTodo.save().then((doc) => {
+//     console.log(JSON.stringify(doc, undefined, 2))
+// }, (e) => {
+//     console.log('There was an error', e)
+// })
+
+
+///  new user model:
+// CHALLANGE
+var newUser = new User({
+    email: 'user1@1.com'
 });
 
-anotherNewTodo.save()
+newUser.save().then((doc) => {
+    console.log(JSON.stringify(doc, undefined, 2))
+}, (e) => {
+    console.log('Another bloddy error', e)
+})
