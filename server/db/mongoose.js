550cb4edf6f6e077821782480fd5d5f5bfde69af
FinlayS <fsgm01@gmail.com>
2018-11-07 20:51:12 +0000

    Set up app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,12 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/TodoApp');
+// mongoose.connect('mongodb://FinlayS:HXqRsznQcMUOgxfI@' +
+//     'cluster0-shard-00-00-gppje.mongodb.net:27017,' +
+//     'cluster0-shard-00-00-gppje.mongodb.net:27017,' +
+//     'cluster0-shard-00-00-gppje.mongodb.net:27017/' +
+//     'todo?ssl=true&replicaSet=' +
+//     'Cluster0-shard-0&authSource=admin');
 
 module.exports = {mongoose};
