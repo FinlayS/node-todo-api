2b760d72dc9355c15cda41bd0b1b7068ae49d219
FinlayS <fsgm01@gmail.com>
2018-11-12 20:55:15 +0000

    S8 L92 - hashing passwords

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: '1012wws'
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(`token: ${token}`);
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log('hash', hash)
+    })
+})
 
-var decoded = jwt.verify(token, '123abc')
-console.log('decoded:', decoded);
+var hashedPassword = '$2a$10$/OV1QGRDayeTGEWR.83rROlbTAD/FVXWdKfiLnK8zJEm14gjTYggq'
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res)
+})
+
+// var data = {
+//     id: '1012wws'
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(`token: ${token}`);
+//
+// var decoded = jwt.verify(token, '123abc')
+// console.log('decoded:', decoded);
 
 // var message ='I am user number 33';
 // var hash = SHA256(message).toString();
